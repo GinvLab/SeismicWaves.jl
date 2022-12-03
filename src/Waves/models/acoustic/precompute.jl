d9877dfdd13348fc4e5079d4fc02013679b808e8
Giacomo Aloisi <giacomo.aloisi1998@gmail.com>
2022-12-03 15:53:24 +0100

    Rename factors

--- src/Waves/models/acoustic/precompute.jl
+++ src/Waves/models/acoustic/precompute.jl
@@ -4,8 +4,8 @@
 Precomputes factors for 2D for isotropic acoustic 2D models.
 """
 @views function precompute_fact!(::IsotropicAcoustic, model::WaveModel2D)
-    model.fact_dx .= model.vel .^ 2 .* (model.dt^2 / model.dx^2)
-    model.fact_dz .= model.vel .^ 2 .* (model.dt^2 / model.dz^2)
+    model.fact_x .= model.vel .^ 2 .* (model.dt^2 / model.dx^2)
+    model.fact_z .= model.vel .^ 2 .* (model.dt^2 / model.dz^2)
 end
 
 """
@@ -14,7 +14,7 @@ end
 Precomputes factors for isotropic acoustic 3D models.
 """
 @views function precompute_fact!(::IsotropicAcoustic, model::WaveModel3D)
-    model.fact_dx .= model.vel .^ 2 .* (model.dt^2 / model.dx^2)
-    model.fact_dy .= model.vel .^ 2 .* (model.dt^2 / model.dy^2)
-    model.fact_dz .= model.vel .^ 2 .* (model.dt^2 / model.dz^2)
+    model.fact_x .= model.vel .^ 2 .* (model.dt^2 / model.dx^2)
+    model.fact_y .= model.vel .^ 2 .* (model.dt^2 / model.dy^2)
+    model.fact_z .= model.vel .^ 2 .* (model.dt^2 / model.dz^2)
 end
