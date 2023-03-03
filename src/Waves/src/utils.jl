69f8ac2dcb95291f521f22c09134677b6d7c2d57
GiackAloZ <giacomo.aloisi1998@gmail.com>
2023-03-03 10:44:12 +0100

    Add gaussian source

--- src/Waves/src/utils.jl
+++ src/Waves/src/utils.jl
@@ -3,6 +3,11 @@
 
 Ricker source time function for current time `t`, activation time `t0` and dominating frequency `f0`.
 """
-function rickersource1D(t::Real, t0::Real, f0::Real)
-    return (1 - 2 * (pi * f0 * (t - t0))^2) * exp(-((pi * f0 * (t - t0))^2))
-end
+rickersource1D(t::Real, t0::Real, f0::Real) = (1 - 2 * (pi * f0 * (t - t0))^2) * exp(-(pi * f0 * (t - t0))^2)
+
+@doc raw"""
+    gaussource1D(t::Real, t0::Real, f0::Real)
+
+Gaussian source time function for current time `t`, activation time `t0` and dominating frequency `f0`.
+"""
+gaussource1D(t::Real, t0::Real, f0::Real) = -(pi * f0 * (t - t0))^2 * exp(-(pi * f0 * (t - t0))^2)
