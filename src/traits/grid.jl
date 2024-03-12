d1526217b91196f152fd2851a14d74bffbf823d2
GiackAloZ <giacomo.aloisi1998@gmail.com>
2024-03-12 13:45:24 +0100

    Remove MPI dep for now

--- src/traits/grid.jl
+++ src/traits/grid.jl
@@ -1,4 +1,4 @@
-using MPI: MPI
+# using MPI: MPI
 
 """
 Abstract trait for a general finite difference grid.
@@ -23,8 +23,8 @@ GridTrait(::Type) = error("GridTrait not implemented for type $(x)")
 
 get_maximum_func(model::WaveSimul) = get_maximum_func(GridTrait(model))
 get_maximum_func(::LocalGrid) = Base.maximum
-get_maximum_func(::GlobalGrid) = (x -> (max_l = maximum(x); MPI.Allreduce(max_l, MPI.MAX, MPI.COMM_WORLD)))
+# get_maximum_func(::GlobalGrid) = (x -> (max_l = maximum(x); MPI.Allreduce(max_l, MPI.MAX, MPI.COMM_WORLD)))
 
 get_minimum_func(model::WaveSimul) = get_minimum_func(GridTrait(model))
 get_minimum_func(::LocalGrid) = Base.minimum
-get_minimum_func(::GlobalGrid) = (x -> (min_l = minimum(x); MPI.Allreduce(min_l, MPI.MIN, MPI.COMM_WORLD)))
+# get_minimum_func(::GlobalGrid) = (x -> (min_l = minimum(x); MPI.Allreduce(min_l, MPI.MIN, MPI.COMM_WORLD)))
