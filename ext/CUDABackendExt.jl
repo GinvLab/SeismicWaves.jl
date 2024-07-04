5b3926341aa2b8f15c74155b680ef1a5e0ef0341
GiackAloZ <giacomo.aloisi1998@gmail.com>
2024-07-04 16:44:00 +0200

    Fix union for GPU backend

--- ext/CUDABackendExt.jl
+++ ext/CUDABackendExt.jl
@@ -11,10 +11,10 @@ include("../src/models/acoustic/backends/Acoustic1D_VD_CPML_GPU.jl")
 include("../src/models/acoustic/backends/Acoustic2D_VD_CPML_GPU.jl")
 
 # Overload backend selection
-SeismicWaves.select_backend(::CPMLBoundaryCondition, ::LocalGrid, ::Type{<:AcousticCDCPMLWaveSimulation{Union{Float16, Float32, Float64}, 1}}, ::Type{Val{:GPU}}) = Acoustic1D_CD_CPML_GPU
-SeismicWaves.select_backend(::CPMLBoundaryCondition, ::LocalGrid, ::Type{<:AcousticCDCPMLWaveSimulation{Union{Float16, Float32, Float64}, 2}}, ::Type{Val{:GPU}}) = Acoustic2D_CD_CPML_GPU
-SeismicWaves.select_backend(::CPMLBoundaryCondition, ::LocalGrid, ::Type{<:AcousticCDCPMLWaveSimulation{Union{Float16, Float32, Float64}, 3}}, ::Type{Val{:GPU}}) = Acoustic3D_CD_CPML_GPU
-SeismicWaves.select_backend(::CPMLBoundaryCondition, ::LocalGrid, ::Type{<:AcousticVDStaggeredCPMLWaveSimulation{Union{Float16, Float32, Float64}, 1}}, ::Type{Val{:GPU}}) = Acoustic1D_VD_CPML_GPU
-SeismicWaves.select_backend(::CPMLBoundaryCondition, ::LocalGrid, ::Type{<:AcousticVDStaggeredCPMLWaveSimulation{Union{Float16, Float32, Float64}, 2}}, ::Type{Val{:GPU}}) = Acoustic2D_VD_CPML_GPU
+SeismicWaves.select_backend(::CPMLBoundaryCondition, ::LocalGrid, ::Type{<:AcousticCDCPMLWaveSimulation{<:Union{Float16, Float32, Float64}, 1}}, ::Type{Val{:GPU}}) = Acoustic1D_CD_CPML_GPU
+SeismicWaves.select_backend(::CPMLBoundaryCondition, ::LocalGrid, ::Type{<:AcousticCDCPMLWaveSimulation{<:Union{Float16, Float32, Float64}, 2}}, ::Type{Val{:GPU}}) = Acoustic2D_CD_CPML_GPU
+SeismicWaves.select_backend(::CPMLBoundaryCondition, ::LocalGrid, ::Type{<:AcousticCDCPMLWaveSimulation{<:Union{Float16, Float32, Float64}, 3}}, ::Type{Val{:GPU}}) = Acoustic3D_CD_CPML_GPU
+SeismicWaves.select_backend(::CPMLBoundaryCondition, ::LocalGrid, ::Type{<:AcousticVDStaggeredCPMLWaveSimulation{<:Union{Float16, Float32, Float64}, 1}}, ::Type{Val{:GPU}}) = Acoustic1D_VD_CPML_GPU
+SeismicWaves.select_backend(::CPMLBoundaryCondition, ::LocalGrid, ::Type{<:AcousticVDStaggeredCPMLWaveSimulation{<:Union{Float16, Float32, Float64}, 2}}, ::Type{Val{:GPU}}) = Acoustic2D_VD_CPML_GPU
 
 end
