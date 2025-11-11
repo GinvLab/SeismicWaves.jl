40156c0aa50626a15372786402b1cec9e97d93e2
GiackAloZ <giacomo.aloisi1998@gmail.com>
2025-11-11 16:22:20 +0100

    Add some @propagate_inbounds for safety

--- src/models/elastic/backends/shared/freesurface_derivatives.jl
+++ src/models/elastic/backends/shared/freesurface_derivatives.jl
@@ -10,66 +10,66 @@ end
 
 # Formula #1 of H-AFDA formulation from Kristek et al. (2002)
 # `f0` is the value of the function at the free surface, which must be half a grid point above/below the (i,j) point
-function formula1_x_left(f, i, j, _Δx, f0)
+Base.@propagate_inbounds function formula1_x_left(f, i, j, _Δx, f0)
     return (-352/105 * f0 + 35/8 * f[i, j] - 35/24 * f[i+1, j] + 21/40 * f[i+2, j] - 5/56 * f[i+3, j]) * _Δx
 end
-function formula1_x_right(f, i, j, _Δx, f0)
+Base.@propagate_inbounds function formula1_x_right(f, i, j, _Δx, f0)
     return ( 352/105 * f0 - 35/8 * f[i, j] + 35/24 * f[i-1, j] - 21/40 * f[i-2, j] + 5/56 * f[i-3, j]) * _Δx
 end
-function formula1_y_top(f, i, j, _Δz, f0)
+Base.@propagate_inbounds function formula1_y_top(f, i, j, _Δz, f0)
     return (-352/105 * f0 + 35/8 * f[i, j] - 35/24 * f[i, j+1] + 21/40 * f[i, j+2] - 5/56 * f[i, j+3]) * _Δz
 end
-function formula1_y_bottom(f, i, j, _Δz, f0)
+Base.@propagate_inbounds function formula1_y_bottom(f, i, j, _Δz, f0)
     return ( 352/105 * f0 - 35/8 * f[i, j] + 35/24 * f[i, j-1] - 21/40 * f[i, j-2] + 5/56 * f[i, j-3]) * _Δz
 end
 
 # Formula #2 of H-AFDA formulation from Kristek et al. (2002)
-function formula2_x_left(f, i, j, _Δx)
+Base.@propagate_inbounds function formula2_x_left(f, i, j, _Δx)
     return (-11/12 * f[i, j] + 17/24 * f[i+1, j] + 3/8 * f[i+2, j] - 5/24 * f[i+3, j] + 1/24 * f[i+4, j]) * _Δx
 end
-function formula2_x_right(f, i, j, _Δx)
+Base.@propagate_inbounds function formula2_x_right(f, i, j, _Δx)
     return ( 11/12 * f[i, j] - 17/24 * f[i-1, j] - 3/8 * f[i-2, j] + 5/24 * f[i-3, j] - 1/24 * f[i-4, j]) * _Δx
 end
-function formula2_y_top(f, i, j, _Δz)
+Base.@propagate_inbounds function formula2_y_top(f, i, j, _Δz)
     return (-11/12 * f[i, j] + 17/24 * f[i, j+1] + 3/8 * f[i, j+2] - 5/24 * f[i, j+3] + 1/24 * f[i, j+4]) * _Δz
 end
-function formula2_y_bottom(f, i, j, _Δz)
+Base.@propagate_inbounds function formula2_y_bottom(f, i, j, _Δz)
     return ( 11/12 * f[i, j] - 17/24 * f[i, j-1] - 3/8 * f[i, j-2] + 5/24 * f[i, j-3] - 1/24 * f[i, j-4]) * _Δz
 end
 
 # Formula #3 of H-AFDA formulation from Kristek et al. (2002)
 # `∂f0` is the first derivative of the function at the free surface, which must be 3 half points above/below the (i,j) point
-function formula3_x_left(f, i, j, _Δx, ∂f0)
+Base.@propagate_inbounds function formula3_x_left(f, i, j, _Δx, ∂f0)
     return (-1/22 * ∂f0) + (-577/528 * f[i-1, j] + 201/176 * f[i, j] - 9/176 * f[i+1, j] + 1/528 * f[i+2, j]) * _Δx
 end
-function formula3_x_right(f, i, j, _Δx, ∂f0)
+Base.@propagate_inbounds function formula3_x_right(f, i, j, _Δx, ∂f0)
     return ( 1/22 * ∂f0) + ( 577/528 * f[i+1, j] - 201/176 * f[i, j] + 9/176 * f[i-1, j] - 1/528 * f[i-2, j]) * _Δx
 end
-function formula3_y_top(f, i, j, _Δz, ∂f0)
+Base.@propagate_inbounds function formula3_y_top(f, i, j, _Δz, ∂f0)
     return (-1/22 * ∂f0) + (-577/528 * f[i, j-1] + 201/176 * f[i, j] - 9/176 * f[i, j+1] + 1/528 * f[i, j+2]) * _Δz
 end
-function formula3_y_bottom(f, i, j, _Δz, ∂f0)
+Base.@propagate_inbounds function formula3_y_bottom(f, i, j, _Δz, ∂f0)
     return ( 1/22 * ∂f0) + ( 577/528 * f[i, j+1] - 201/176 * f[i, j] + 9/176 * f[i, j-1] - 1/528 * f[i, j-2]) * _Δz
 end
 
 # Formula #4 of H-AFDA formulation from Kristek et al. (2002)
 # `f0` is the value of the function at the free surface, which must be 3 half points above/below the (i,j) point
-function formula4_x_left(f, i, j, _Δx, f0)
+Base.@propagate_inbounds function formula4_x_left(f, i, j, _Δx, f0)
     return (16/105 * f0 - 31/24 * f[i-1, j] + 29/24 * f[i, j] - 3/40 * f[i+1, j] + 1/168 * f[i+2, j]) * _Δx
 end
-function formula4_x_right(f, i, j, _Δx, f0)
+Base.@propagate_inbounds function formula4_x_right(f, i, j, _Δx, f0)
     return (-16/105 * f0 + 31/24 * f[i+1, j] - 29/24 * f[i, j] + 3/40 * f[i-1, j] - 1/168 * f[i-2, j]) * _Δx
 end
-function formula4_y_top(f, i, j, _Δz, f0)
+Base.@propagate_inbounds function formula4_y_top(f, i, j, _Δz, f0)
     return (16/105 * f0 - 31/24 * f[i, j-1] + 29/24 * f[i, j] - 3/40 * f[i, j+1] + 1/168 * f[i, j+2]) * _Δz
 end
-function formula4_y_bottom(f, i, j, _Δz, f0)
+Base.@propagate_inbounds function formula4_y_bottom(f, i, j, _Δz, f0)
     return (-16/105 * f0 + 31/24 * f[i, j+1] - 29/24 * f[i, j] + 3/40 * f[i, j-1] - 1/168 * f[i, j-2]) * _Δz
 end
 
 # Wrapper functions to select the appropriate formula based on the field of which we are computing the derivative
 
-function ∂σxx∂x_4th(σxx, i, j, _Δx, nx)
+Base.@propagate_inbounds function ∂σxx∂x_4th(σxx, i, j, _Δx, nx)
     if i == 1
         # Set missing left point to 0
         return ∂x4th_inner(0, σxx[i, j], σxx[i+1, j], σxx[i+2, j], _Δx)
@@ -82,7 +82,7 @@ function ∂σxx∂x_4th(σxx, i, j, _Δx, nx)
     end
 end
 
-function ∂σzz∂z_4th(σzz, i, j, _Δz, nz, freeboundtop)
+Base.@propagate_inbounds function ∂σzz∂z_4th(σzz, i, j, _Δz, nz, freeboundtop)
     if j == 1
         if freeboundtop
             # Free surface at top boundary, half a grid point above
@@ -100,7 +100,7 @@ function ∂σzz∂z_4th(σzz, i, j, _Δz, nz, freeboundtop)
     end
 end
 
-function ∂σxz∂x_4th(σxz, i, j, _Δz, nx)
+Base.@propagate_inbounds function ∂σxz∂x_4th(σxz, i, j, _Δz, nx)
     if i == 1
         # Set missing left points to 0
         return ∂x4th_inner(0, 0, σxz[i, j], σxz[i+1, j], _Δz)
@@ -120,7 +120,7 @@ function ∂σxz∂x_4th(σxz, i, j, _Δz, nx)
     end
 end
 
-function ∂σxz∂z_4th(σxz, i, j, _Δz, nz, freeboundtop)
+Base.@propagate_inbounds function ∂σxz∂z_4th(σxz, i, j, _Δz, nz, freeboundtop)
     if j == 1
         if freeboundtop
             # Free surface at top boundary, half a grid point above
@@ -152,7 +152,7 @@ function ∂σxz∂z_4th(σxz, i, j, _Δz, nz, freeboundtop)
     end
 end
 
-function ∂ux∂x_4th(ux, i, j, _Δx, nx)
+Base.@propagate_inbounds function ∂ux∂x_4th(ux, i, j, _Δx, nx)
     if i == 1
         # Set missing left points to 0
         return ∂x4th_inner(0, 0, ux[i, j], ux[i+1, j], _Δx)
@@ -172,7 +172,7 @@ function ∂ux∂x_4th(ux, i, j, _Δx, nx)
     end
 end
 
-function ∂uz∂z_4th(ux, uz, λ, μ, i, j, _Δx, _Δz, nx, nz, freeboundtop)
+Base.@propagate_inbounds function ∂uz∂z_4th(ux, uz, λ, μ, i, j, _Δx, _Δz, nx, nz, freeboundtop)
     if j == 1
         if freeboundtop
             # On the free surface at top boundary, use ∂ux∂x to compute ∂uz∂z via Hooke's law and free surface condition
@@ -206,7 +206,7 @@ function ∂uz∂z_4th(ux, uz, λ, μ, i, j, _Δx, _Δz, nx, nz, freeboundtop)
     end
 end
 
-function ∂ux∂z_4th(ux, i, j, _Δz, nz, freeboundtop)
+Base.@propagate_inbounds function ∂ux∂z_4th(ux, i, j, _Δz, nz, freeboundtop)
     if j == 1
         if freeboundtop
             # Free surface at top boundary, half a grid point above
@@ -224,7 +224,7 @@ function ∂ux∂z_4th(ux, i, j, _Δz, nz, freeboundtop)
     end
 end
 
-function ∂uz∂x_4th(uz, i, j, _Δx, nx)
+Base.@propagate_inbounds function ∂uz∂x_4th(uz, i, j, _Δx, nx)
     if i == 1
         # Set missing left point to 0
         return ∂x4th_inner(0, uz[i, j], uz[i+1, j], uz[i+2, j], _Δx)
