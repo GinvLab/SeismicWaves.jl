22e545fe9896580497911ad34ab55e2713e00ad0
GiackAloZ <giacomo.aloisi1998@gmail.com>
2025-11-26 10:57:36 +0100

    Working free surface gradients! 2nd order accurate + stress mirroring scheme

--- src/models/elastic/backends/shared/correlate_gradient_xPU.jl
+++ src/models/elastic/backends/shared/correlate_gradient_xPU.jl
@@ -1,31 +1,41 @@
-@parallel function correlate_gradient_ρ_kernel!(grad_ρ, adjcur, uold, ucur, unew, _dt2)
-    @all(grad_ρ) = @all(grad_ρ) + (@all(adjcur) * (@all(uold) - 2 * @all(ucur) + @all(unew)) * _dt2)
+@parallel_indices (i, j) function correlate_gradient_ρ_kernel!(grad_ρ, adjcur, uold, ucur, unew, _dt2, freeboundtop)
+    if j == 1 && freeboundtop
+        # Half contribution at free surface
+        grad_ρ[i, j] += (adjcur[i, j] * (uold[i, j] - 2 * ucur[i, j] + unew[i, j]) * _dt2) / 2
+    else
+        grad_ρ[i, j] += (adjcur[i, j] * (uold[i, j] - 2 * ucur[i, j] + unew[i, j]) * _dt2)
+    end
 
     return nothing
 end
 
 @parallel_indices (i, j) function correlate_gradient_λ_μ_kernel!(grad_λ, grad_μ, adjux, adjuz, ux, uz, λ, μ, _Δx, _Δz, nx, nz, freeboundtop)
     # Compute bulk strain tensor components
-    ε_xx     = ∂ux∂x_4th(ux, i, j, _Δx, nx)
-    ε_xx_adj = ∂ux∂x_4th(adjux, i, j, _Δx, nx)
-    ε_zz     = ∂uz∂z_4th(ux   , uz   , λ, μ, i, j, _Δx, _Δz, nx, nz, freeboundtop)
-    ε_zz_adj = ∂uz∂z_4th(adjux, adjuz, λ, μ, i, j, _Δx, _Δz, nx, nz, freeboundtop)
+    ε_xx     = ∂ux∂x_2nd(ux, i, j, _Δx, nx)
+    ε_xx_adj = ∂ux∂x_2nd(adjux, i, j, _Δx, nx)
+    ε_zz     = ∂uz∂z_2nd(ux   , uz   , λ, μ, i, j, _Δx, _Δz, nx, nz, freeboundtop)
+    ε_zz_adj = ∂uz∂z_2nd(adjux, adjuz, λ, μ, i, j, _Δx, _Δz, nx, nz, freeboundtop)
     # Compute divergence of displacement
     div_u = ε_xx + ε_zz
     div_u_adj = ε_xx_adj + ε_zz_adj
     # Accumulate gradients
-    grad_λ[i, j] += div_u * div_u_adj
-    grad_μ[i, j] += 2 * (ε_xx * ε_xx_adj + ε_zz * ε_zz_adj)
+    if j == 1 && freeboundtop # Half contribution at free surface
+        grad_λ[i, j] += div_u * div_u_adj / 2
+        grad_μ[i, j] += (ε_xx * ε_xx_adj + ε_zz * ε_zz_adj)
+    else
+        grad_λ[i, j] += div_u * div_u_adj
+        grad_μ[i, j] += 2 * (ε_xx * ε_xx_adj + ε_zz * ε_zz_adj)
+    end
 
     return nothing
 end
 
 @parallel_indices (i, j) function correlate_gradient_μ_ihalf_jhalf_kernel!(grad_μ_ihalf_jhalf, adjux, adjuz, ux, uz, _Δx, _Δz, nx, nz, freeboundtop)
     # Compute shear strain tensor components
-    ε_xz     = (∂uz∂x_4th(uz   , i, j, _Δx, nx) + 
-                ∂ux∂z_4th(ux   , i, j, _Δz, nz, freeboundtop)) / 2
-    ε_xz_adj = (∂uz∂x_4th(adjuz, i, j, _Δx, nx) + 
-                ∂ux∂z_4th(adjux, i, j, _Δz, nz, freeboundtop)) / 2
+    ε_xz     = (∂uz∂x_2nd(uz   , i, j, _Δx, nx) + 
+                ∂ux∂z_2nd(ux   , i, j, _Δz, nz, freeboundtop)) / 2
+    ε_xz_adj = (∂uz∂x_2nd(adjuz, i, j, _Δx, nx) + 
+                ∂ux∂z_2nd(adjux, i, j, _Δz, nz, freeboundtop)) / 2
     ε_zx = ε_xz
     ε_zx_adj = ε_xz_adj
     # Accumulate gradients
@@ -36,17 +46,19 @@ end
 
 function correlate_gradients!(grid, uold_corr, ucur_corr, unew_corr, dt, freeboundtop)
     nx, nz = grid.size
-    @parallel correlate_gradient_ρ_kernel!(
+    @parallel (1:nx-1, 1:nz) correlate_gradient_ρ_kernel!(
         grid.fields["grad_ρ_ihalf"].value,
         grid.fields["adjucur"].value[1],
         uold_corr[1], ucur_corr[1], unew_corr[1],
-        1 / dt^2
+        1 / dt^2,
+        freeboundtop
     )
-    @parallel correlate_gradient_ρ_kernel!(
+    @parallel (1:nx, 1:nz-1) correlate_gradient_ρ_kernel!(
         grid.fields["grad_ρ_jhalf"].value,
         grid.fields["adjucur"].value[2],
         uold_corr[2], ucur_corr[2], unew_corr[2],
-        1 / dt^2
+        1 / dt^2,
+        false
     )
     idxσxx = freeboundtop ? (1:nz-1) : (2:nz-1)
     @parallel (2:nx-1, idxσxx) correlate_gradient_λ_μ_kernel!(
